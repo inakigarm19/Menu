f0ff76b07d8902d66ac5ace2a4e6ff2b04e2475d
AlexisGM2806 <alexisgimenezmartinez@gmail.com>
2025-09-02 20:41:35 -0300

    Clase Waiter

--- src/Waiter.cs
+++ src/Waiter.cs
@@ -2,11 +2,24 @@ using System.Collections.Generic;
 
 namespace Ucu.Poo.Restaurant
 {
-    /// <summary>
-    /// Representa un mozo en el restaurante, encargado de atender mesas.
-    /// </summary>
     public class Waiter
     {
+        public string Name;
         private List<Table> assignedTables = new List<Table>();
+
+        public Waiter(string name)
+        {
+            Name = name;
+        }
+
+        public void AssignTable(Table table)
+        {
+            assignedTables.Add(table);
+        }
+
+        public void TakeOrder(Table table, Dish dish)
+        {
+            table.AddDish(dish); // Añada el platillo, cambiar a nombre del metodo que tenga Table
+        }
     }
 }
